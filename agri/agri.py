00b8d46cdb91a18a606fc72a550a7d90f3bef906
dan <lordaniel24@gmail.com>
2024-04-10 22:53:28 -0500

    kv split

--- agri/agri.py
+++ agri/agri.py
@@ -8,36 +8,36 @@ from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.clock import Clock    
 from kivy.lang import Builder
+from kivy.config import Config 
+from kivy.core.window import Window
+
 
 from time import strftime   
 
-
-
-
 class AgriStatusScreen(Screen):
-    pass
+    Builder.load_file('agri/kvs/agristatusscreen.kv')
 
 class AgriSettingsScreen(Screen):
-    pass
+    Builder.load_file('agri/kvs/agrisettingsscreen.kv')
 
 class AgriPowerScreen(Screen):
-    pass
+    Builder.load_file('agri/kvs/agripowerscreen.kv')
 
 class AgriVarScreen(Screen):
-    pass
+    Builder.load_file('agri/kvs/agrivarscreen.kv')
 
 class AgriChecksScreen(Screen):
-    pass
+    Builder.load_file('agri/kvs/agrichecksscreen.kv')
 
 class AgriMainWindow(Screen):
-    pass
+    Builder.load_file('agri/kvs/agrimainwindow.kv')
 
 class AgriWindowManager(ScreenManager):
     pass
 
 
 class AgriApp(App):
-
+    
     # could not for the life of me get the timer to work.
 
     # for the clock in the settings page
@@ -48,5 +48,8 @@ class AgriApp(App):
     #   Clock.schedule_interval(self.update, 0)
 
     def build(self):
+    
+        Window.size = (1024, 600)
+
         kv = Builder.load_file('agri/kvs/agriwindowmanager.kv')
         return kv
